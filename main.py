b7b8f5dd481de2f4854cdf76220a1cc72e8e2ac8
teal5221 <mys.alter10@gmail.com>
2025-11-16 19:38:24 +0900

    [Fix]ぱす修正

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import discord
 
-from minecraft_discord_controller import settings
+from minecraft_discord_controller.config import settings
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("minecraft_discord_controller")
